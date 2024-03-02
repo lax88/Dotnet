f024daea039cbf3e2c882dbcb7687fd78cc4fbe7
Laxmi Shrestha <102451303+lax88@users.noreply.github.com>
2024-03-02 16:01:16 +0545

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Basic program to write "hello world"
+            Console.WriteLine("Hello World!");//prints the value on the cosole output
+            Console.ReadKey();//used to hold the screen and exits after a key is pressed
+        }
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //basic program to read key from user and display it in console
+            Console.WriteLine("Enter a key:");
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+            Console.Write("You entered the key " +  keyInfo.KeyChar);
+            Console.ReadKey();
+        }
+    }
+}
